119e2473e22d448f3ff8fae9d1dd025601f713c2
mwaqasashfaq <55546816+mwaqasashfaq@users.noreply.github.com>
2019-10-04 19:30:04 +0500

    Delete star_print_using_for_loop.py

--- star_print_using_for_loop.py
+++
@@ -1,9 +0,0 @@
-for star in range(0,50):
-    for star2 in range(0,star+1):
-        print("*",end="")
-    print()
-
-for star in range(0,50):
-    for star2 in range(50,star,-1):
-        print("*",end="")
-    print()
